6d45c90f94812b669d3ae2b52866ab354507ac84
sweta <sweeta.gumanju@gmail.com>
2026-06-23 12:17:27 +0545

    thirdrubyprogram.rb

--- assignments/thirdrubyprogram.rb
+++ assignments/thirdrubyprogram.rb
@@ -1,1 +1,8 @@
 # list 10 things that could be represented with arrays
+
+quotes = Array.new
+puts "There are #{quotes.length} quotes now."
+
+things = ["plant","book","house","charger","windows","phone","restaurant","ruby","garden","hometown"]
+
+puts things
