48e096a4118c50da6518d1d14cf37dd7740ca46e
sweta <sweeta.gumanju@gmail.com>
2026-06-23 14:10:40 +0545

    completed hash assignment

---
+++ assignments/fourthrubyprogram.rb
@@ -0,0 +1,31 @@
+# create a hash consisting of 5 countries
+# for each country, store the population, GDP per capita, and capital
+  
+countries = {
+ "Nepal" => {
+    population: 20000000,
+    gdp_per_capital: "xyz",
+    capital: "Kathmandu"
+  },
+ "India" => {
+    population: 50000000,
+    gdp_per_capital: "abc",
+    capital: "New Delhi"
+  },
+ "Australia" => {
+    population: 30000000,
+    gdp_per_capital: "abcd",
+    capital: "Canberra"
+  },
+ "Japan" => {
+    population: 20000000,
+    gdp_per_capital: "cdef",
+    capital: "Tokyo"
+  },
+ "Afhganistan" => {
+    population: 20000000,
+    gdp_per_capital: "abc",
+    capital: "Kabul"
+  },
+}
+puts countries
