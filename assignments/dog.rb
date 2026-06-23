4c45f8cb974512f8f866a61a564c05ad3f10d64f
sweta <sweeta.gumanju@gmail.com>
2026-06-23 16:42:11 +0545

    new instance

--- assignments/dog.rb
+++ assignments/dog.rb
@@ -1,6 +1,11 @@
 class Dog 
+  def initialize(name, breed)
+    @name = name 
+    @breed = breed
+  end
 end
 
-barnie = Dog.new
+# Pass "Barnie" as the name, and "Golden Retriever" as the breed
+barnie = Dog.new("Barnie", "Golden Retriever")
 
 puts barnie.inspect
