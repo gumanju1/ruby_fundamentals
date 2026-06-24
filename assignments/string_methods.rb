f40563e5dfc0e3b6eeb3a4da79b2bfbcd4cc329c
sweta <sweeta.gumanju@gmail.com>
2026-06-24 10:58:19 +0545

    string_methods.rb

---
+++ assignments/string_methods.rb
@@ -0,0 +1,28 @@
+# Makes all letters uppercase
+puts "this is a string".upcase
+
+# Makes all letters lowercase
+puts "THIS IS A STRING".downcase
+
+# Get length of string 
+puts "this is a string".length
+
+# Reverse string
+puts "this is a string".reverse
+
+# Capitalize first letter
+puts "this is a string".capitalize
+
+# Check if string exists within string
+puts "this is a string".include?("ri")
+
+puts "this is a string".split("")
+
+# Split string into an array of individual characters
+puts "this is a string".split("")
+
+# Convert string into integer
+puts "10".to_i
+
+# Convert string into float
+puts "10".to_f
