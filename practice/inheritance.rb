63dcefb60b098fde314304d4ae13ace3c451455f
sweta <sweeta.gumanju@gmail.com>
2026-06-24 17:03:52 +0545

    inheritance

---
+++ practice/inheritance.rb
@@ -0,0 +1,27 @@
+class Truck
+  def speed_up
+    puts "I'm speeding up!"
+  end
+
+  def speed_down
+    puts "I'm slowing down!"
+  end
+
+  def print_type
+    puts "I'm a truck!"
+  end
+end
+
+  class PoliceCar
+    def speed_up
+      puts "I'm speeding up!"
+    end
+    
+    def speed_down
+      puts "I'm slowing down!"
+    end
+
+    def print_type
+      puts "I'm a police car!"
+    end
+  end
